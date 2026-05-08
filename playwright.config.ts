7e769c498961abc60b2144f8dd63e89e73fe00b0
cb-sagarwanjari <sagar.wanjari@chargebee.com>
2026-05-08 13:06:29 +0530

    support to allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,11 +21,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]], 
+  reporter: [['list'],['html'],['allure-playwright']],
   globalTeardown: require.resolve('./ai/globalTeardown'),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
